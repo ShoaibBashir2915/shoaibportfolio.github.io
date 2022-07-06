7b52d738586a013f900dbcb68019c44aebafcf9d
Shoaib Bashir <97108876+ShoaibBashir2915@users.noreply.github.com>
2022-07-06 10:34:44 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function menuOpen (){
+let menu = document.querySelector('.menu');
+
+menu.classList.toggle('open');
+
+let list = document.querySelector('.list');
+list.classList.toggle('open');
+};
+
+
+
+let close = document.querySelector('menu.open');
